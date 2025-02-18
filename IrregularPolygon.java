421be4e23679836f10c43ee1b1de6d73b8d4e50c
SohhumSinha <sohhumcs@gmail.com>
2025-02-18 18:31:16 +0000

    Day 1 - Errors/Troubleshoot/Complete successful code

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -14,27 +14,51 @@ public class IrregularPolygon {
     // public methods
     public void add(Point2D.Double aPoint)
     {
-        // TODO: Add a point to the IrregularPolygon.
+        myPolygon.add(aPoint);
     }
 
     public double perimeter() {
-        // TODO: Calculate the perimeter.
-        return 3.14;
+        if (myPolygon.size() < 2) return 0.0;
+
+        double sum = 0.0;
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double start = myPolygon.get(i);
+            Point2D.Double end = myPolygon.get((i + 1) % myPolygon.size());
+            sum += start.distance(end);
+        }
+        return sum;
     }
 
     public double area() {
-        // TODO: Calculate the area.
-        return 0.0;
+        int n = myPolygon.size();
+        if (n < 3) return 0.0;
+    
+        double area = 0.0;
+        for (int i = 0; i < n; i++) {
+            Point2D.Double p1 = myPolygon.get(i);
+            Point2D.Double p2 = myPolygon.get((i + 1) % n);
+            area += p1.x * p2.y - p2.x * p1.y;
+        }
+        return Math.abs(area / 2.0);
     }
 
     public void draw()
     {
         // Wrap the DrawingTool in a try/catch to allow development without need for graphics.
         try {
-            // TODO: Draw the polygon.
-            // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
             DrawingTool pen = new DrawingTool(new SketchPad(500, 500));
-            pen.move(50, 50);
+        if (myPolygon.size() > 0) {
+            Point2D.Double first = myPolygon.get(0);
+            pen.up();
+            pen.move(first.x, first.y);
+            pen.down();
+
+            for (int i = 1; i < myPolygon.size(); i++) {
+                Point2D.Double point = myPolygon.get(i);
+                pen.move(point.x, point.y);
+            }
+            pen.move(first.x, first.y); 
+        }
         } catch (java.awt.HeadlessException e) {
             System.out.println("Exception: No graphics support available.");
         }
